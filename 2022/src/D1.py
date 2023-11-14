614a7691091ae832d328cf9215b46d12b438bdd9
arthur-H2W33 <150786520+arthur-H2W33@users.noreply.github.com>
2023-11-14 14:45:44 +0100

    Create D1.py

---
+++ 2022/src/D1.py
@@ -0,0 +1,20 @@
+path="C:\\Users\\apot\\Desktop\\AdventOfCode\\2022\\inputs\\"
+
+IPTraw=open(path+'inputd1.txt').read().split("\n")
+IPT=IPTraw
+
+
+compile=[]
+compte=0
+for elt in IPT:
+    if elt!='':
+            compte+=int(elt)
+    else:
+        compile.append(compte)
+        compte=0
+
+compile.sort()
+
+print("Q1", compile[-1])        
+
+print("Q2", compile[-1]+compile[-2]+compile[-3])        
