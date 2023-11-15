a7c8955ba3ce03ecd2ac48b8d3ba531ea4ab1b2e
arthur-H2W33 <150786520+arthur-H2W33@users.noreply.github.com>
2023-11-15 11:16:40 +0100

    Update 2k22D09.py

--- 2022/src/2k22D09.py
+++ 2022/src/2k22D09.py
@@ -7,8 +7,8 @@ Created on Sat Dec  3 11:28:32 2022
 
 path="C:\\Users\\apot\\Desktop\\AdventOfCode\\2022\\inputs\\"
 
-IPTraw=open(path+'inputd9.txt').read().split("\n")
-test=open(path+'inputd9ex.txt').read().split("\n")
+IPTraw=open(path+'D9.txt').read().split("\n")
+test=open(path+'D9ex.txt').read().split("\n")
 IPT=IPTraw
 
 commandes=[elt.split(" ") for elt in IPT]
